45e0f6f839b81706c4eb647bcac0acb8d9f51107
dinei84 <devdinei@gmail.com>
2025-03-20 17:15:44 -0300

    Atualizado Program

--- guerreiros/src/application/Program.java
+++ guerreiros/src/application/Program.java
@@ -12,16 +12,18 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         Saiajin goku = new Saiajin("Sayajin", 1000, "Kamehameha", 2, "Planeta Vegita", "Goku");
-        goku.trasnformacao(3);
+        Saiajin vegita = new Saiajin("Sayajin", 1000, "Alvorecer do Dia", 2, "Planeta Vegita", "Vegita");
 
         Namecuseijin picolo = new Namecuseijin("Namecusaijim", 5000, "Aquencosapo", 1200, "Regeneração", "Picollo");
         picolo.setFundir(3500);
 
         Humanos kuririn = new Humanos("Humanos", 3500, "Kiensan", "Kuririn", "Kame");
+        Humanos iancha = new Humanos("Humanos", 4200, "Golpe do Lobo", "Iancha", "Kame");
 
         System.out.println(goku);
         System.out.println(picolo);
         System.out.println(kuririn);
+        System.out.println(iancha);
 
         sc.close();
     }
