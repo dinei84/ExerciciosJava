619e90eeb2ba668a0bbb9f0115eceec8810d3665
dinei84 <devdinei@gmail.com>
2025-03-20 21:14:50 -0300

    Cadastrando mais guerreiros z

--- guerreiros/src/application/Program.java
+++ guerreiros/src/application/Program.java
@@ -13,17 +13,29 @@ public class Program {
 
         Saiajin goku = new Saiajin("Sayajin", 1000, "Kamehameha", 2, "Planeta Vegita", "Goku");
         Saiajin vegita = new Saiajin("Sayajin", 1000, "Alvorecer do Dia", 2, "Planeta Vegita", "Vegita");
+        Saiajin gohan = new Saiajin("Saiajin", 2500, "Mazenko", 2, "Terra", "Gohan");
+        Saiajin tranksdofuturo = new Saiajin("Saiajin", 2000, "Ataque de Espada Brilhante", 1, "Futuro", "Trunks");
 
         Namecuseijin picolo = new Namecuseijin("Namecusaijim", 5000, "Aquencosapo", 1200, "Regeneração", "Picollo");
-        picolo.setFundir(3500);
+        Namecuseijin dende = new Namecuseijin("Namecuseijin", 100, "Não possui", 200, "Cura", "Dende");
 
         Humanos kuririn = new Humanos("Humanos", 3500, "Kiensan", "Kuririn", "Kame");
         Humanos iancha = new Humanos("Humanos", 4200, "Golpe do Lobo", "Iancha", "Kame");
+        Humanos tenshinhan = new Humanos("Humanos", 5000, "Kikohan", "Tenshinhan", "Tsuru-Sen'nin");
 
         System.out.println(goku);
+        System.out.println(vegita);
+        System.out.println(gohan);
+        System.out.println(tranksdofuturo);
+
+
         System.out.println(picolo);
+        System.out.println(dende);
+
+
         System.out.println(kuririn);
         System.out.println(iancha);
+        System.out.println(tenshinhan);
 
         sc.close();
     }
