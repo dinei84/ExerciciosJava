3e5d753b066444089df04a444f00334fb394cfea
Claudinei Almeida Souza <devdinei@gmail.com>
2025-04-04 21:54:10 -0300

    Criado algumas pastas

---
+++ aula.excecoes.personalizadas/src/model/entities/Reservas.java
@@ -0,0 +1,12 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Reservas {
+    
+    private Integer numeroQuarto;
+    private Date checkIn;
+    private Date checkOut;
+    
+
+}
