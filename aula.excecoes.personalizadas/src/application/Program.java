c71400cf1c1c12d9f9772eb7150cd923314ca693
Claudinei Almeida Souza <devdinei@gmail.com>
2025-04-04 22:43:41 -0300

    Falta a personalização das Exceptions

--- aula.excecoes.personalizadas/src/application/Program.java
+++ aula.excecoes.personalizadas/src/application/Program.java
@@ -1,5 +1,49 @@
 package application;
 
+import model.entities.Reservas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            System.out.print("Numero do quarto: ");
+            int numero = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Data do Check-in: ");
+            Date checkIn = sdf.parse(sc.next());
+            System.out.print("Dara do Check-out: ");
+            Date checkout = sdf.parse(sc.next());
+
+            Reservas reserva = new Reservas(numero, checkIn, checkout);
+            System.out.println("Reservas: " + reserva);
+
+            System.out.println();
+            System.out.println("Entre com a data atualizada: ");
+            System.out.print("Data do Check-in: ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Data do Check-out: ");
+            checkout = sdf.parse(sc.next());
+
+            reserva.atualizaData(checkIn, checkout);
+            System.out.println("Reserva: " + reserva);
+
+        }catch (ParseException e){
+            System.out.println("Formato da data invalido!");
+        }catch (IllegalArgumentException e){
+            System.out.println("Erro na reserva: " + e.getMessage());
+        }
+
+
+
+
+        sc.close();
+    }
 
 }
--- aula.excecoes.personalizadas/src/model/entities/Reservas.java
+++ aula.excecoes.personalizadas/src/model/entities/Reservas.java
@@ -1,12 +1,66 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservas {
     
     private Integer numeroQuarto;
     private Date checkIn;
     private Date checkOut;
-    
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservas(Integer numeroQuarto, Date checkIn, Date checkOut) {
+        this.numeroQuarto = numeroQuarto;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getNumeroQuarto() {
+        return numeroQuarto;
+    }
+
+    public void setNumeroQuarto(Integer numeroQuarto) {
+        this.numeroQuarto = numeroQuarto;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+
+    public long duracao(){
+        long diferenca = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
+    }
+
+    public void atualizaData(Date checkIn, Date checkOut){
+        Date agora = new Date();
+        if (checkIn.before(agora) || checkOut.before(agora)){
+            throw new IllegalArgumentException("As datas de reserva para atualizações devem ser datas futuras");
+        }
+        if (!checkOut.after(checkIn)){
+            throw new IllegalArgumentException("A data deve ser posterior à data de check-in");
+        }
+
+
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        return "Quarto " +
+                "\nNumero do Quarto: " + numeroQuarto +
+                "\nCheck-in: " + sdf.format(checkIn) +
+                "\nCheck-out: " + sdf.format(checkOut) +
+                "\nDuração: " + duracao() + " noites";
+    }
 
 }
