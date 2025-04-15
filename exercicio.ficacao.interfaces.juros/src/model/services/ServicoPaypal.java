b5fa174ac56386820c6c4a5c00b9d008eaad65ea
Claudinei Almeida Souza <devdinei@gmail.com>
2025-04-15 20:47:31 -0300

    Trabalhando no exercicio de juros

---
+++ exercicio.ficacao.interfaces.juros/src/model/services/ServicoPaypal.java
@@ -0,0 +1,17 @@
+package model.services;
+
+public class ServicoPaypal implements ServicoPagamentoOnLine{
+
+    @Override
+    public Double taxaDePagamento(Double quantia) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'taxaDePagamento'");
+    }
+
+    @Override
+    public Double juros(Double quantia, Integer meses) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'juros'");
+    }
+
+}
