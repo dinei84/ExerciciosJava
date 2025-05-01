7b72fd78a12ae0e6ee6ac03869a1107b8eeb572a
Claudinei Almeida Souza <devdinei@gmail.com>
2025-05-01 18:37:21 -0300

    Terminado eercicios de Default Methods

--- aula.default.methods/src/services/BrasilInterestService.java
+++ aula.default.methods/src/services/BrasilInterestService.java
@@ -1,4 +1,17 @@
 package services;
 
-public class BrasilInterestService {
+import java.security.InvalidParameterException;
+
+public class BrasilInterestService implements InterestService{
+    private double interestRate;
+
+    public BrasilInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+
 }
--- aula.default.methods/src/services/UsaInterestService.java
+++ aula.default.methods/src/services/UsaInterestService.java
@@ -2,21 +2,17 @@ package services;
 
 import java.security.InvalidParameterException;
 
-public class BrasilInterestService {
+public class UsaInterestService implements InterestService{
     private double interestRate;
 
-    public BrasilInterestService(double interestRate) {
+    public UsaInterestService(double interestRate) {
         this.interestRate = interestRate;
     }
 
+    @Override
     public double getInterestRate() {
         return interestRate;
     }
 
-    public double payment(double amount, int months){
-        if (months < 1){
-            throw new InvalidParameterException("Months munst be greater than zero");
-        }
-        return amount * Math.pow(1.0 + interestRate / 100.0, months);
-    }
+
 }
