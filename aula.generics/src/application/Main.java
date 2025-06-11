9f4e8baa32d9f36f94c5955bd34a03d80271ffda
dinei84 <devdinei@gmail.com>
2025-06-11 09:12:03 -0300

    Alterações

--- aula.generics/src/application/Main.java
+++ aula.generics/src/application/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
         PrintService<String> ps = new PrintService<>();
 
         System.out.print("How many values?: ");
